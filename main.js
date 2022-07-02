620d12f8ba52613d85814cbde5dc4bbcc457a223
reedbyford <reedbyford@gmail.com>
2022-07-02 05:18:16 -0500

    Edited styling

--- main.js
+++ main.js
@@ -8,4 +8,21 @@ function openLink(){
 const elmHamburgers = document.querySelectorAll('.smurf');
 elmHamburgers.forEach(elem => elem.addEventListener('click', openLink));
 
+// ==UserScript==
+// @name         Station Signout
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automagically signout of stations
+// @author       bbbyford
+// @match        http://ar-appserver-kaf5-node3.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=10448&connectToShalo=false
+// @grant        none
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+$(document).ready(function() {  
+    document.querySelector('button').click();
+});
+
 
