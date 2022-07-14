02fd3c7169d5fd89b72e3d9999b7327f402905b8
reedbyford <reedbyford@gmail.com>
2022-07-14 01:49:50 -0500

    Fiddling with JS

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-document.querySelector('.smurf').addEventListener('click', openLink)
+document.querySelector('.station').addEventListener('click', openLink)
+// 
+// function openLink(){
+//     let numGrabber = document.querySelector('.smurf').textContent
+//     window.open(`http://ar-appserver-kaf5-node3.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=${numGrabber}&connectToShalo=false`, '_blank')
+// }
+// 
+// const elmHamburgers = document.querySelectorAll('.smurf');
+// elmHamburgers.forEach(elem => elem.addEventListener('click', openLink));
+
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder().
+   withCapabilities(webdriver.Capabilities.chrome()).
+   build();
+
 
 function openLink(){
-    let numGrabber = document.querySelector('.smurf').textContent
-    window.open(`http://ar-appserver-kaf5-node3.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=${numGrabber}&connectToShalo=false`, '_blank')
+    window.onload = function() {
+        driver.get('http://ar-appserver-kaf5-node2.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=10432&connectToShalo=false');
+        driver.findElement(webdriver.By.name('button')).click();
+      };
+    // driver.wait(function() {
+    //  return driver.getTitle().then(function(title) {
+    //    return title === 'webdriver - Google Search';
+    //  });
+    // }, 1000);
 }
-
-const elmHamburgers = document.querySelectorAll('.smurf');
-elmHamburgers.forEach(elem => elem.addEventListener('click', openLink));
-
-// ==UserScript==
-// @name         Station Signout
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Automagically signout of stations
-// @author       bbbyford
-// @match        http://ar-appserver-kaf5-node3.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=10448&connectToShalo=false
-// @grant        none
-// @require http://code.jquery.com/jquery-latest.js
-// ==/UserScript==
-/* jshint -W097 */
-'use strict';
-
-$(document).ready(function() {  
-    document.querySelector('button').click();
-});
+// driver.get('http://ar-appserver-kaf5-node2.kaf5.amazon.com:9348/webapp/nicomachus.html?customer=AMZN&warehouse=KAFW&zone=agKivaA01&id=10432&connectToShalo=false');
+// driver.findElement(webdriver.By.name('button')).click();
+// // driver.wait(function() {
+// //  return driver.getTitle().then(function(title) {
+// //    return title === 'webdriver - Google Search';
+// //  });
+// // }, 1000);
+// 
+// driver.quit();
 
 
